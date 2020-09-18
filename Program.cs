f556a56a968c2f99958b491c5b7996b7a197307a
Mikael Pajari <mikael.pajari@live.se>
2020-09-18 14:28:20 +0200

    klar

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace fibb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Fibnum = new List<int> {1, 1};
+                while(Fibnum.Count < 20)
+                {
+                    var prev = Fibnum[Fibnum.Count -1];
+                    var prev2 = Fibnum[Fibnum.Count -2];
+                    Fibnum.Add(prev + prev2);
+                }
+            
+                    foreach (var item in Fibnum)
+                    {
+                        Console.WriteLine(item);
+                    }
+
+        }
+    }
+}
